e883fb14be2fb1650834520d7900b54fe55f8952
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-06-04 21:05:16 -0700

    Added button for zone loading;

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -6,6 +6,17 @@ import $ from 'jquery';
 import logo from './images/grow_dammit.jpg'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isZoneLoaded:false
+    }
+    this.handleZoneLoader = this.handleZoneLoader.bind(this);
+  }
+
+  handleZoneLoader(){
+  alert('Zone loaded');
+  }
 
 
   componentDidMount(){
@@ -24,6 +35,11 @@ class App extends React.Component {
     return (
       <div className="logo">
       <img src={logo} width="25%" height="25%" alt="logo"/>
+         <div className = 'zone-button'>
+         <button onClick = {this.handleZoneLoader}>
+        Load zones
+         </button>
+         </div>
          </div>
     );
 
